2b22a407dac4476533ad49c97179080b769daaba
Michel-Rooney <michelrooney16@gmail.com>
2023-05-15 13:40:08 -0300

    First Commit

---
+++ member/models.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Comment(models.Model):
+    creator =  models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ManyToManyField('self')
+    likes = models.IntegerField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+
+    def __str__(self) -> str:
+        return self.creator
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.IntegerField()
+    comments = models.ManyToManyField(Comment)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+
+    def __str__(self) -> str:
+        return self.name
+    
