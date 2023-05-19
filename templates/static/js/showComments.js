1a3665b48567daf8718c95403063daf3f080be1d
Juan <juansoussev@gmail.com>
2023-05-19 19:59:35 -0300

    show and hidden comments of a post parcially ok

---
+++ templates/static/js/showComments.js
@@ -0,0 +1,13 @@
+const btnResponse = document.querySelector('.response')
+const textArea = document.querySelector('.form')
+const cancel = document.querySelector('.cancel')
+btnResponse.addEventListener('click', ()=> {
+    console.log('clicou carai')
+    if ( textArea.classList.contains('d-none') ) {
+        console.log('tem o d none')
+        textArea.classList.remove('d-none')
+    }
+})
+cancel.addEventListener('click', ()=>{
+    textArea.classList.add('d-none')
+})
