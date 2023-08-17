85bbef0408550e0899d42095509ccff55c119396
juan <juansoussev@gmail.com>
2023-08-16 22:28:39 -0300

    update da responsividade da aplicação

--- templates/static/js/chageTheme.js
+++ templates/static/js/chageTheme.js
@@ -3,7 +3,7 @@ const main = document.querySelector('main')
 const btnChangeMoon = document.querySelector('.change-moon')
 const btnChangeSun = document.querySelector('.change-sun')
 
-btnMode.addEventListener('click', ()=> {
+btnMode.addEventListener('click', () => {
     if (main.classList.contains('dark-mode')) {
 
         main.classList.remove('dark-mode')
@@ -11,24 +11,24 @@ btnMode.addEventListener('click', ()=> {
         btnMode.classList.remove('bi-brightness-high')
         btnMode.classList.add('bi-moon-stars')
     } else {
-        
+
         main.classList.remove('ligth-mode')
         main.classList.add('dark-mode')
-        btnMode.classList.remove('bi-moon-stars')  
+        btnMode.classList.remove('bi-moon-stars')
         btnMode.classList.add('bi-brightness-high')
     }
 })
 
-btnChangeMoon.addEventListener('click', ()=> {
+btnChangeMoon.addEventListener('click', () => {
     main.classList.remove('ligth-mode')
     main.classList.add('dark-mode')
     btnMode.classList.remove('bi-brightness-high')
     btnMode.classList.add('bi-moon-stars')
 })
 
-btnChangeSun.addEventListener('click', ()=> {
+btnChangeSun.addEventListener('click', () => {
     main.classList.remove('dark-mode')
     main.classList.add('ligth-mode')
-    btnMode.classList.remove('bi-moon-stars')  
+    btnMode.classList.remove('bi-moon-stars')
     btnMode.classList.add('bi-brightness-high')
 })
